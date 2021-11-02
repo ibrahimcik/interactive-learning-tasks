8831aa131b4e315c8d82b6e1e6432f1ced7eb7d0
ibrahim <ibrahimzaferkahveci@gmail.com>
2021-11-02 21:39:49 +0000

    user added to group

---
+++ iam/iam.tf
@@ -0,0 +1,31 @@
+resource "aws_iam_user" "bob" {
+  name = "bob"
+  path = "/system/"
+
+  tags = {
+    Team = "DevOps"
+  }
+}
+
+resource "aws_iam_group" "sysusers" {
+  name = "sysusers"
+  path = "/users/"
+  
+}
+
+resource "aws_iam_group_membership" "team" {
+  name = "member"
+
+  users = [
+    aws_iam_user.bob.name
+  ]
+
+  group = aws_iam_group.sysusers.name
+}
+
+output "aws_iam_user" {
+  value = aws_iam_user.bob.name
+}
+output "aws_iam_group" {
+  value = aws_iam_group.sysusers.name
+}
